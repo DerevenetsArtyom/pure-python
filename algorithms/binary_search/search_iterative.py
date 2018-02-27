10459f07bedd636a6f62e4359a49d72dbacae079
DerevenetsArtyom <artem@comvoy.be>
2018-02-27 12:57:35 +0200

    Added 'binary_search_iterative'

---
+++ algorithms/binary_search/search_iterative.py
@@ -0,0 +1,39 @@
+# Iterative solution
+# Function takes only two arguments that I mostly prefer
+
+
+def binary_search_iterative(arr, item):
+    first = 0
+    last = len(arr) - 1
+
+    while first <= last:
+        mid_index = (first + last) // 2
+        if item < arr[mid_index]:
+            last = mid_index - 1
+        elif item > arr[mid_index]:
+            first = mid_index + 1
+        else:
+            return mid_index
+
+    return -1
+
+
+# General cases
+assert binary_search_iterative([1, 2, 3, 4, 5, 6, 7, 8], 5) == 4
+assert binary_search_iterative([1, 2, 3, 4, 5, 6, 7, 8], 3) == 2
+
+# First and last elements of list
+assert binary_search_iterative([1, 2, 3, 4, 5, 6, 7, 8], 1) == 0
+assert binary_search_iterative([1, 2, 3, 4, 5, 6, 7, 8], 8) == 7
+
+# Not found
+assert binary_search_iterative([1, 2, 3, 4, 5, 6, 7, 8], 0) == -1
+
+# Empty list
+assert binary_search_iterative([], 1) == -1
+
+# With one element
+assert binary_search_iterative([1], 1) == 0
+
+assert binary_search_iterative([1, 2], 1) == 0
+assert binary_search_iterative([1, 2], 2) == 1
