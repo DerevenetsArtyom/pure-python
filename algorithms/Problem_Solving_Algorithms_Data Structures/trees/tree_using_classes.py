af1f5267e77a87b15795076f3a54a213268af123
DerevenetsArtyom <artem@comvoy.be>
2018-03-15 17:24:06 +0200

    Added 'preorder' func

--- algorithms/Problem_Solving_Algorithms_Data Structures/trees/tree_using_classes.py
+++ algorithms/Problem_Solving_Algorithms_Data Structures/trees/tree_using_classes.py
@@ -23,3 +23,27 @@ class BinaryTree:
             binary_tree.right_child = self.right_child  # move existing node
             self.right_child = binary_tree  # assign created tree as right child
 
+    def preorder(self):
+        """Could be also as a method, but pretty useless in that version"""
+        print(self.root)
+        if self.left_child:
+            self.left_child.preorder()
+        if self.right_child:
+            self.right_child.preorder()
+
+
+r = BinaryTree('a')
+
+r.insert_left('b')
+r.insert_left(7)
+
+r.insert_right('c')
+r.insert_right(6)
+
+
+def preorder(tree: BinaryTree):
+    """Обход в прямом порядке"""
+    if tree:
+        print(tree.root)
+        preorder(tree.left_child)
+        preorder(tree.right_child)
