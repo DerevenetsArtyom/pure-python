bc10573bb03ead0bed07499d833dcf2be0722e0f
DerevenetsArtyom <artem@comvoy.be>
2018-03-16 11:53:37 +0200

    Added 'del_min' to Binary Heap

--- algorithms/Problem_Solving_Algorithms_Data Structures/trees/binary_heap.py
+++ algorithms/Problem_Solving_Algorithms_Data Structures/trees/binary_heap.py
@@ -24,6 +24,24 @@ class BinaryHeap:
         self._move_up(self.size)
 
 
+    def del_min(self):
+        old_root = self.heap_list[1]
+        last_element = self.heap_list[-1]  # or self.heapList[self.currentSize]
+
+        # move last element to the top to support heap's structure property
+        self.heap_list[1] = last_element
+        self.heap_list.pop()  # TODO assign to last element
+        self.size -= 1
+
+        # try to return property of orderliness, cause it broken
+        self._move_down(1)
+        return old_root
+
+
+
+
+
+
 bh = BinaryHeap()
 bh.insert(5)
 bh.insert(7)
