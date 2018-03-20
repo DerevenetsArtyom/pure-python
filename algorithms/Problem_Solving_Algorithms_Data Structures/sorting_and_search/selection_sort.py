25e6ad3ca55658f95b3bad2709e0dd230158a200
DerevenetsArtyom <artem@comvoy.be>
2018-03-20 12:39:33 +0200

    Added 'selection_sort_right_order'

--- algorithms/Problem_Solving_Algorithms_Data Structures/sorting_and_search/selection_sort.py
+++ algorithms/Problem_Solving_Algorithms_Data Structures/sorting_and_search/selection_sort.py
@@ -1,8 +1,8 @@
-def selection_sort(arr):
+def selection_sort_reverse_order(arr):
     """Sort the mutable sequence seq in place and return it."""
     for right_index in range(len(arr) - 1, 0, -1):
         index_of_max = 0
-        # Find the index of greatest item in seq[:i+1].
+        # Find the index of greatest item in seq[:i+1]
         for i in range(1, right_index + 1):
             if arr[i] > arr[index_of_max]:
                 index_of_max = i
@@ -11,7 +11,22 @@ def selection_sort(arr):
             arr[index_of_max], arr[right_index]
 
 
+def selection_sort_right_order(arr):
+    for i in range(len(arr)):
+        min_index = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[min_index], arr[i] = arr[i], arr[min_index]
+
+
 alist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-selection_sort(alist)
+selection_sort_reverse_order(alist)
 
 assert alist == [17, 20, 26, 31, 44, 54, 55, 77, 93]
+
+
+blist = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+selection_sort_right_order(blist)
+
+assert blist == [17, 20, 26, 31, 44, 54, 55, 77, 93]
